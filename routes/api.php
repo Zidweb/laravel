3419498db743ec882212afc2c4623a6c40d6e7a0
CaddyDz <caddydz4@gmail.com>
2020-11-18 11:56:11 +0100

    Installed Sanctum
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request): User {
-	return $request->user();
-});
+Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
