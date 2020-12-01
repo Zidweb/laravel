bfe49c9babb867065322d8d00bee61d18064123c
Salim Djerbouh <caddydz4@gmail.com>
2020-12-01 18:38:30 +0100

    remove cloud option
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- config/filesystems.php
+++ config/filesystems.php
@@ -19,19 +19,6 @@ return [
 
 	/*
 	|--------------------------------------------------------------------------
-	| Default Cloud Filesystem Disk
-	|--------------------------------------------------------------------------
-	|
-	| Many applications store files both locally and in the cloud. For this
-	| reason, you may specify a default "cloud" driver here. This driver
-	| will be bound as the Cloud disk implementation in the container.
-	|
-	*/
-
-	'cloud' => env('FILESYSTEM_CLOUD', 's3'),
-
-	/*
-	|--------------------------------------------------------------------------
 	| Filesystem Disks
 	|--------------------------------------------------------------------------
 	|
@@ -53,7 +40,7 @@ return [
 		'public' => [
 			'driver' => 'local',
 			'root' => storage_path('app/public'),
-			'url' => env('APP_URL').'/storage',
+			'url' => env('APP_URL') . '/storage',
 			'visibility' => 'public',
 		],
 
