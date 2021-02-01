e12e0d0ed4b0340bc511050a06a68839d998ed3e
Salim Djerbouh <caddydz4@gmail.com>
2021-02-01 14:25:37 +0100

    Soft delete model
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Laravel\Scout\Searchable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -38,7 +39,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
-	use HasApiTokens, HasFactory, Notifiable, Searchable;
+	use HasApiTokens, HasFactory, Notifiable, Searchable, SoftDeletes;
 
 	/**
 	 * The attributes that are mass assignable.
