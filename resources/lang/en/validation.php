5030bdb743fd442dc9c33b1023c9321d24a917d2
Salim Djerbouh <caddydz4@gmail.com>
2021-03-20 17:31:31 +0100

    Add prohibited validation rule
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -106,6 +106,7 @@ return [
 	'required_with_all' => 'The :attribute field is required when :values are present.',
 	'required_without' => 'The :attribute field is required when :values is not present.',
 	'required_without_all' => 'The :attribute field is required when none of :values are present.',
+	'prohibited' => 'The :attribute field is prohibited.',
 	'same' => 'The :attribute and :other must match.',
 	'size' => [
 		'numeric' => 'The :attribute must be :size.',
